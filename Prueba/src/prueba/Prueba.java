0900fbc9106c5457d8a19831575a247a67159c94
barquilla97 <36697564+barquilla97@users.noreply.github.com>
2018-02-23 09:51:16 +0100

    Update Prueba.java

--- Prueba/src/prueba/Prueba.java
+++ Prueba/src/prueba/Prueba.java
@@ -5,7 +5,7 @@ public class Prueba {
 
     public static void main(String[] args) {
         
-        System.out.println("Holi Nachi Barqui, klk");
+        System.out.println("Holi Nachi Barquijjjjjjjjjj, klk");
     }
     
 }
